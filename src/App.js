3adb38a0ff77083ef49416b2146b3e07229777c0
mayur890 <mayurparmar890890@gmail.com>
2023-08-12 12:46:27 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { useEffect } from "react";
+import "./App.css";
+
+function App() {
+  useEffect(() => {
+    console.log("Mayur");
+  }, []);
+  return (
+    <>
+      <div></div>
+    </>
+  );
+}
+
+export default App;
